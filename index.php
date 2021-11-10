e417358f08939648aa1df8945bfbc594719533d6
Ilyass940 <92728686+Ilyass940@users.noreply.github.com>
2021-11-10 11:52:17 +0100

    Ajout de mes fichiers essentiels

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AppleTV</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="" href="js/bootstrap.js">
+</head>
+<body>
+    <div class="container-fluid"><!--Contenu global-->
+
+        <!-- Nos coups de coeur -->
+        <div class="row titre"><!--Ligne-->
+            <div class="col">
+                <h3>Nos coup de coeur</h3>
+            </div>
+            <div class="col text-end">
+                <p class="tout-afficher"><a href="#">Tout afficher</a></p>
+            </div>
+        </div>
+
+        <div class="row coups-de-coeur-liste"><!-- Coup de coueur liste -->
+            <div class="col"><!-- Coups de coeur 1 -->
+                <div class="row">
+                    <div class="col">
+                        <img class="img-fluid affiche" src="img/coup-de-coeur-1.jpg" alt="Coup de coeur film">
+                    </div>
+                    <div class="col">
+                        <p class="category text-secondary text-uppercase">Catégorie</p>
+                        <h5>Titre</h5>
+                        <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ducimus sed molestiae repudiandae illum fuga deleniti at veritatis maiores.
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col"><!-- Coups de coeur 2 -->
+                <div class="row">
+                    <div class="col">
+                        <img class="img-fluid affiche" src="img/coup-de-coeur-2.jpg" alt="Coup de coeur série">
+                    </div>
+                    <div class="col">
+                        <p class="category text-secondary text-uppercase">Catégorie</p>
+                        <h5>Titre</h5>
+                        <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ducimus sed molestiae repudiandae illum fuga deleniti at veritatis maiores.
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col"><!-- Coups de coeur 3 -->
+                <div class="row">
+                    <div class="col">
+                        <img class="img-fluid affiche" src="img/coup-de-coeur-3.jpg" alt="Coup de coeur documentaire">
+                    </div>
+                    <div class="col">
+                        <p class="category text-secondary text-uppercase">Catégorie</p>
+                        <h5>Titre</h5>
+                        <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ducimus sed molestiae repudiandae illum fuga deleniti at veritatis maiores.
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr/>
+
+    </div>
+
+
+</body>
+</html>
